6bae1635e3484edda03f93319b595210be427f18
nasty <nasty@saybert.ru>
2021-08-04 19:12:04 +0300

    extended Java Comparator

--- src/JavaTests.java
+++ src/JavaTests.java
@@ -4,25 +4,105 @@ import java.util.*;
 
 public class JavaTests {
     public static void main(String[] args) {
-        Map<Person,String> map = new HashMap<>();
-        Set<Person> set = new HashSet<>();
+        List<String> animals = new ArrayList<>();
+        animals.add("cat");
+        animals.add("dog");
+        animals.add("z");
+        animals.add("ab");
+        animals.add("bird");
 
-        Person person1 = new Person(1,"Mike");
-        Person person2 = new Person(1,"Mike");
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(5);
+        numbers.add(0);
+        numbers.add(500);
+        numbers.add(100);
 
-        map.put(person1, "123");
-        map.put(person2, "123");
+        Collections.sort(animals,new StringLengthComparator());
+        System.out.println(animals);
 
-        set.add(person1);
-        set.add(person2);
+        System.out.println(numbers);
+        //Collections.sort(numbers,new BackwardsIntsComparator());
 
-        System.out.println(map);
-        System.out.println(set);
+        //то же самое.
+        //если надо вызвать 1 раз, можно реализовать через анонимный класс, как показано ниже
+
+        Collections.sort(numbers, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if(o1>o2){
+                    return -1;
+                } else if (o1<o2){
+                    return 1;
+                } else return 0;
+            }
+        });
+
+        //или так, через лямбда
+        Collections.sort(numbers, (o1, o2) -> {
+            if(o1>o2){
+                return -1;
+            } else if (o1<o2){
+                return 1;
+            } else return 0;
+        });
+        System.out.println(numbers);
+
+        List<Person> people = new ArrayList<>();
+        people.add(new Person(3,"Katy"));
+        people.add(new Person(1,"Mike"));
+        people.add(new Person(2,"Bob"));
+
+        Collections.sort(people, new Comparator<Person>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                if (o1.getId()>o2.getId()){
+                    return 1;
+                } else if (o1.getId()<o2.getId()){
+                    return -1;
+                } else return 0;
+            }
+        });
+        System.out.println(people);
 
     }
 }
 
-class Person {
+class StringLengthComparator implements Comparator<String>{
+
+    @Override
+    public int compare(String o1, String o2) {
+        /*
+        Если    о1 > o2 -> 1
+                о1 < o2 -> -1
+                о1 = o2 -> 0
+         */
+        if(o1.length()>o2.length()){
+            return 1;
+        } else if (o1.length()<o2.length()){
+            return -1;
+        } else return 0;
+    }
+}
+
+class BackwardsIntsComparator implements Comparator<Integer>{
+
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        /*
+        Если    о1 > o2 -> 1
+                о1 < o2 -> -1
+                о1 = o2 -> 0
+         */
+        if(o1>o2){
+            return -1;
+        } else if (o1<o2){
+            return 1;
+        } else return 0;
+    }
+}
+
+
+class Person{
     private int id;
     private String name;
 
@@ -31,28 +111,19 @@ class Person {
         this.name = name;
     }
 
-    public String toString(){
-        return "Person{" + id + ", name =" + name + "}";
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Person person = (Person) o;
-        return id == person.id &&
-                name.equals(person.name);
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
     }
-    // преобразование объекта произвольной длины в строку/число фиксированной длины
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name);
-    }
-    /* Контракт Hashcode-equals:
-    Сравнили 2 объекта через хэш:
-    1. хэши разные = объекты точно разные (быстрая операция)
-    2. хеши совпадают ->
-        либо однаковые, либо только их хэши (коллизия хеширования):
-        2.1. вызываем equals (медленная операция) -> получаем однозначный результат
-     */
 }
