1b234433f0190647327da5b6cbb9b2011d36c1db
nasty <nasty@saybert.ru>
2021-08-04 19:40:27 +0300

    extended Java Comparable

--- src/JavaTests.java
+++ src/JavaTests.java
@@ -4,105 +4,31 @@ import java.util.*;
 
 public class JavaTests {
     public static void main(String[] args) {
-        List<String> animals = new ArrayList<>();
-        animals.add("cat");
-        animals.add("dog");
-        animals.add("z");
-        animals.add("ab");
-        animals.add("bird");
-
-        List<Integer> numbers = new ArrayList<>();
-        numbers.add(5);
-        numbers.add(0);
-        numbers.add(500);
-        numbers.add(100);
-
-        Collections.sort(animals,new StringLengthComparator());
-        System.out.println(animals);
-
-        System.out.println(numbers);
-        //Collections.sort(numbers,new BackwardsIntsComparator());
-
-        //то же самое.
-        //если надо вызвать 1 раз, можно реализовать через анонимный класс, как показано ниже
-
-        Collections.sort(numbers, new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                if(o1>o2){
-                    return -1;
-                } else if (o1<o2){
-                    return 1;
-                } else return 0;
-            }
-        });
-
-        //или так, через лямбда
-        Collections.sort(numbers, (o1, o2) -> {
-            if(o1>o2){
-                return -1;
-            } else if (o1<o2){
-                return 1;
-            } else return 0;
-        });
-        System.out.println(numbers);
-
         List<Person> people = new ArrayList<>();
-        people.add(new Person(3,"Katy"));
-        people.add(new Person(1,"Mike"));
-        people.add(new Person(2,"Bob"));
+        Set<Person> peopleSet = new TreeSet<>();
 
-        Collections.sort(people, new Comparator<Person>() {
-            @Override
-            public int compare(Person o1, Person o2) {
-                if (o1.getId()>o2.getId()){
-                    return 1;
-                } else if (o1.getId()<o2.getId()){
-                    return -1;
-                } else return 0;
-            }
-        });
+        addElements(people);
+        /*
+        При попытке добавить в TreeSet два элемента, которые, согласно
+        нашему компоратору, равны (здесь - длина имени одинакова),
+        добавится только один элемент, тот, который добавляли раньше
+         */
+        addElements(peopleSet);
+
+        Collections.sort(people);
         System.out.println(people);
+        System.out.println(peopleSet);
+    }
 
+    private static void addElements(Collection collection){
+        collection.add(new Person(4, "Mike"));
+        collection.add(new Person(2, "Bo"));
+        collection.add(new Person(3, "Kathy"));
+        collection.add(new Person(1, "Thomas"));
     }
 }
 
-class StringLengthComparator implements Comparator<String>{
-
-    @Override
-    public int compare(String o1, String o2) {
-        /*
-        Если    о1 > o2 -> 1
-                о1 < o2 -> -1
-                о1 = o2 -> 0
-         */
-        if(o1.length()>o2.length()){
-            return 1;
-        } else if (o1.length()<o2.length()){
-            return -1;
-        } else return 0;
-    }
-}
-
-class BackwardsIntsComparator implements Comparator<Integer>{
-
-    @Override
-    public int compare(Integer o1, Integer o2) {
-        /*
-        Если    о1 > o2 -> 1
-                о1 < o2 -> -1
-                о1 = o2 -> 0
-         */
-        if(o1>o2){
-            return -1;
-        } else if (o1<o2){
-            return 1;
-        } else return 0;
-    }
-}
-
-
-class Person{
+class Person implements Comparable<Person>{
     private int id;
     private String name;
 
@@ -120,10 +46,35 @@ class Person{
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id &&
+                Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
     public String toString() {
-        return "Person{" +
+        return "{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
                 '}';
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        if (this.name.length() > o.getName().length()){
+            return 1;
+        }else if (this.name.length() < o.getName().length()){
+            return -1;
+        } else {
+            return 0;
+        }
     }
 }
