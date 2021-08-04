fad6973abdec04c1409123433c264cbfd6c703c5
nasty <nasty@saybert.ru>
2021-08-04 20:08:17 +0300

    extended Java Stack

--- src/JavaTests.java
+++ src/JavaTests.java
@@ -5,48 +5,20 @@ import java.util.concurrent.ArrayBlockingQueue;
 
 public class JavaTests {
     public static void main(String[] args) {
-        Person person1 = new Person(1);
-        Person person2 = new Person(2);
-        Person person3 = new Person(3);
-        Person person4 = new Person(4);
-
-        //Queue <Person> people = new LinkedList<>();
-
-        Queue <Person> people = new ArrayBlockingQueue<Person>(3);
+        //Queue - FIFO
+        //Stack - LIFO
+        Stack<Integer> stack = new Stack<>();
+        stack.push(5);
+        stack.push(3);
+        stack.push(1);
 
 
-        //add выдаст ошибку, offer вернет спец.значение (false)
-        System.out.println(people.offer(person3));
-        System.out.println(people.offer(person2));
-        System.out.println(people.offer(person4));
-        System.out.println(people.offer(person1));
+//        System.out.println(stack.peek());   //посмотрели
+//        System.out.println(stack.pop());    //посмотрели и удалили
 
-//        people.add(person3);
-//        people.add(person2);
-//        people.add(person4);
-//        people.add(person1);
-//
-//        System.out.println(people.remove());        //посмотрели и удалили
-//        System.out.println(people.peek());          //только посмотрели
-//        System.out.println(people);
+        while (!stack.empty()){
+            System.out.println(stack.pop());
+        }
     }
 }
 
-class Person{
-    private int id;
-
-    public Person(int id) {
-        this.id = id;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-                "id=" + id +
-                '}';
-    }
-}
