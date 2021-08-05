ce4d374bbb43a3e6d27bf572babf00f1556040da
nasty <nasty@saybert.ru>
2021-08-05 13:25:20 +0300

    extended Java Multithreading Thread pool (ExecutorService)

--- src/JavaTests.java
+++ src/JavaTests.java
@@ -1,81 +1,38 @@
 import org.w3c.dom.ls.LSOutput;
 
 import java.util.*;
-import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.*;
 
 public class JavaTests {
-    public static void main(String[] args) {
-        new Worker().main();
+    public static void main(String[] args) throws InterruptedException {
+        //пул потоков
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+        for (int i = 0; i < 5; i++)
+            //передали 5 заданий
+            executorService.submit(new Work(i));
+        //приступаем к выполнению заданий
+        executorService.shutdown();
+        System.out.println("All tasks are submited");
+        //сколько ждать времени до конца исполнения потоков
+        executorService.awaitTermination(1, TimeUnit.DAYS);
     }
 }
 
-class Worker{
-    private List<Integer> list1 = new ArrayList<>();
-    private List<Integer> list2 = new ArrayList<>();
-    Random r = new Random();
+class Work implements Runnable{
+    private int id;
 
-    //выделяем 2 объекта, на которые будем синхронизироваться.
-    // Если занят один - можем взять второй
-    Object lock1 = new Object();
-    Object lock2 = new Object();
-
-    public void addToList1(){
-        synchronized (lock1){
-            try {
-                Thread.sleep(1);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            list1.add(r.nextInt(100));
-        }
+    public Work(int id) {
+        this.id = id;
     }
 
-    public void addToList2(){
-        synchronized (lock2){
-            try {
-                Thread.sleep(1);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            list2.add(r.nextInt(100));
-        }
-    }
-
-    public void work(){
-        for (int i = 0; i <1000; i++){
-            addToList1();
-            addToList2();
-        }
-    }
-
-    public void main(){
-        long before = System.currentTimeMillis();
-        Thread thread1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                work();
-            }
-        });
-        Thread thread2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                work();
-            }
-        });
-        thread1.start();
-        thread2.start();
-
+    @Override
+    public void run() {
         try {
-            thread1.join();
-            thread2.join();
+            Thread.sleep(5000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
-        long after = System.currentTimeMillis();
-        System.out.println("Program took " + (after-before) + " ms to run");
-
-        System.out.println("List1: " + list1.size());
-        System.out.println("List2: " + list2.size());
+        System.out.println("Work " + id + " is completed");
     }
 }
