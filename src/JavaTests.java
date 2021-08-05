a5e58f11d33f1742d530e2d63af66be85244ed06
nasty <nasty@saybert.ru>
2021-08-05 14:09:07 +0300

    extended Java Multithreading Pattern producer-consumer(|)

--- src/JavaTests.java
+++ src/JavaTests.java
@@ -4,35 +4,56 @@ import java.util.*;
 import java.util.concurrent.*;
 
 public class JavaTests {
+    //потокобезоасная очередь с ограниченным сислом элементов
+    private static BlockingQueue<Integer> queue = new ArrayBlockingQueue<>(10);
+
     public static void main(String[] args) throws InterruptedException {
-        //пул потоков
-        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    produce();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
-        for (int i = 0; i < 5; i++)
-            //передали 5 заданий
-            executorService.submit(new Work(i));
-        //приступаем к выполнению заданий
-        executorService.shutdown();
-        System.out.println("All tasks are submited");
-        //сколько ждать времени до конца исполнения потоков
-        executorService.awaitTermination(1, TimeUnit.DAYS);
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    consumer();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+
     }
-}
 
-class Work implements Runnable{
-    private int id;
-
-    public Work(int id) {
-        this.id = id;
-    }
-
-    @Override
-    public void run() {
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+    private static void produce() throws InterruptedException {
+        Random random = new Random();
+        while (true){
+            queue.put(random.nextInt(100));
         }
-        System.out.println("Work " + id + " is completed");
     }
+
+    private static void consumer() throws InterruptedException {
+        Random random = new Random();
+        while (true) {
+            Thread.sleep(100);
+            if(random.nextInt(10)==5){
+                //метод take ждет, пока элементы будут добавлены
+                System.out.println(queue.take());
+                System.out.println("Queue size is " + queue.size());
+            }
+        }
+    }
+
 }
