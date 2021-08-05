a241fd0d951c54dc8c6b4513b11f1f77a58607b8
nasty <nasty@saybert.ru>
2021-08-05 11:39:43 +0300

    extended Java Multithreading

--- src/JavaTests.java
+++ src/JavaTests.java
@@ -4,21 +4,52 @@ import java.util.*;
 import java.util.concurrent.ArrayBlockingQueue;
 
 public class JavaTests {
-    public static void main(String[] args) {
-        //Queue - FIFO
-        //Stack - LIFO
-        Stack<Integer> stack = new Stack<>();
-        stack.push(5);
-        stack.push(3);
-        stack.push(1);
+    public static void main(String[] args) throws InterruptedException {
+        //потоки не синхронизированы, борятся за процессорное время
+//        MyThread myThread = new MyThread();
+//        myThread.start();
+//
+//        Thread.sleep(1000);
+//        MyThread myThread2 = new MyThread();
+//        myThread2.start();
 
+//        System.out.println("I'm going to sleep");
+//        Thread.sleep(3000);
+//        System.out.println("I am awake");
 
-//        System.out.println(stack.peek());   //посмотрели
-//        System.out.println(stack.pop());    //посмотрели и удалили
+        Thread thread = new Thread(new Runner());
+        thread.start();
+        System.out.println("Hello from main thread");
 
-        while (!stack.empty()){
-            System.out.println(stack.pop());
+    }
+}
+
+//лучше делать так
+class Runner implements Runnable{
+
+    @Override
+    public void run() {
+        for (int i = 0; i<10; i++){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Hello from MyThread " + i);
         }
     }
 }
 
+class MyThread extends Thread{
+    public void run(){
+        //Здесь будет выполняться код, исполняемый в текущем потоке
+        for (int i = 0; i<10; i++){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Hello from MyThread " + i);
+        }
+    }
+}
