786e07ec5f050d76d965e8d7709a811d3d5ed943
nasty <nasty@saybert.ru>
2021-08-09 14:41:51 +0300

    extended Java Regex

--- src/JavaTests.java
+++ src/JavaTests.java
@@ -1,131 +1,40 @@
-import java.util.Locale;
-import java.util.PrimitiveIterator;
-import java.util.Random;
-import java.util.concurrent.*;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import javax.crypto.spec.PSource;
 
 public class JavaTests {
-    public static void main(String[] args) throws InterruptedException {
-        Runner runner = new Runner();
-        Thread thread1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                runner.firstThread();
-            }
-        });
+    public static void main(String[] args)  {
+        /*
+        \\d - одна цифра
+        \\w - одна англ буква
+        +   - один или более
+        *   - 0 или более
+        ?   - 0 или 1 символов до..
+        (x|y|z)  - или x, или y, или z. Вариант из множества
+        [a-zA-Z] - все буквы англ алфавита, строчные и прописные
+        [1-9]    - аналогично \\d
+        [^]      - символы, кроме множества
+        .        - абсолютно любой символ
+        {2}      - 2 символа до..
+        {2, }    - от 2х символов до..
+        {2, 7}   - от 2 до 7 символов до..
+         */
 
-        Thread thread2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                runner.secondThread();
-            }
-        });
-        thread1.start();
-        thread2.start();
+        String a = "-1234";
+        String b = "1235";
+        String c = "+12345672";
+        String regex = "[a-zA-Z31]*(-|\\+)?\\d*";
+        System.out.println(a.matches(regex));
+        System.out.println(b.matches(regex));
+        System.out.println(c.matches(regex));
 
-        thread1.join();
-        thread2.join();
+        String d = "afg3311ihjk145678";
+        System.out.println(d.matches(regex));
+        String e = "hello";
+        System.out.println(e.matches("[^abc]*"));
 
-        runner.finished();
-    }
-}
+        String url = "http://www.google.com";
+        String url2 = "http://www.yandex.ru";
+        System.out.println(url.matches("http://www\\..+\\.(com|ru|biz|de)"));
 
-class Runner{
-    private Account account1 = new Account();
-    private Account account2 = new Account();
-
-    private Lock lock1 = new ReentrantLock();
-    private Lock lock2 = new ReentrantLock();
-
-
-    private void takeLocks (Lock lock1, Lock lock2){
-        boolean firstLockTaken = false;
-        boolean secondLockTaken = false;
-        while (true){
-            try{
-                firstLockTaken = lock1.tryLock();
-                secondLockTaken = lock2.tryLock();
-            } finally {
-                if (firstLockTaken && secondLockTaken){
-                    return;
-                }
-                if (firstLockTaken){
-                    lock1.unlock();
-                }
-                if (secondLockTaken){
-                    lock2.unlock();
-                }
-            }
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    public void firstThread(){
-        Random random = new Random();
-
-//         Большая вложенность - плохо
-//        for (int i = 0; i<10000; i++){
-//            //Account.transfer(account1,account2,random.nextInt(100));
-//            synchronized (account1){
-//                synchronized (account2){
-//                    Account.transfer(account1,account2,random.nextInt(100));
-//                }
-//            }
-//        }
-
-        for (int i = 0; i<10000; i++){
-            takeLocks(lock1,lock2);
-            try{
-                Account.transfer(account1,account2,random.nextInt(100));
-            } finally {         //обязательно unlock в finally-блоке
-                lock1.unlock();
-                lock2.unlock();
-            }
-        }
-    }
-
-    public void secondThread(){
-        Random random = new Random();
-        for (int i = 0; i<10000; i++){
-            takeLocks(lock1,lock2);
-            try{
-                Account.transfer(account2,account1,random.nextInt(100));
-            } finally {
-                lock1.unlock();
-                lock2.unlock();
-            }
-        }
-    }
-
-    public void finished(){
-        System.out.println(account1.getBallance());
-        System.out.println(account2.getBallance());
-        System.out.println("Total ballance: " + (account1.getBallance()+account2.getBallance()));
-    }
-}
-
-class Account{
-    private int ballance = 10000;
-
-    public void deposit (int amount){
-        ballance +=amount;
-    }
-
-    public void withdraw (int amount){
-        ballance-=amount;
-    }
-
-    public int getBallance() {
-        return ballance;
-    }
-
-    public static void transfer(Account account1, Account account2, int amount){
-        account1.withdraw(amount);
-        account2.deposit(amount);
+        System.out.println(b.matches("\\d{2,}"));
     }
 }
