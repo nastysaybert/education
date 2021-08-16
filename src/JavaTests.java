3abc147fbada1b6ae380a9a6f2704b89e2accffe
nasty <nasty@saybert.ru>
2021-08-16 14:07:55 +0300

    extended Java Regex Pattern & Matcher

--- src/JavaTests.java
+++ src/JavaTests.java
@@ -1,29 +1,23 @@
+import com.sun.source.tree.WhileLoopTree;
+
 import javax.crypto.spec.PSource;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class JavaTests {
     public static void main(String[] args)  {
-        /*
-        \\d - одна цифра
-        \\w - одна англ буква
-        +   - один или более
-        *   - 0 или более
-        ?   - 0 или 1 символов до..
-        (x|y|z)  - или x, или y, или z. Вариант из множества
-        [a-zA-Z] - все буквы англ алфавита, строчные и прописные
-        [1-9]    - аналогично \\d
-        [^]      - символы, кроме множества
-        .        - абсолютно любой символ
-        {2}      - 2 символа до..
-        {2, }    - от 2х символов до..
-        {2, 7}   - от 2 до 7 символов до..
-         */
-        String a = "Hello there hey";
-        //String[] words = a.split("[1-9]+");
+       String text = "Текст – это несколько предложений, " +
+               "связанных a@yandex.ru между собой по смыслу и грамматически. " +
+               "Есть и более «сложная» tom@mail.com формулировка данного определения. " +
+               "Текст – это john@gmail.com последовательное объединение предложений, " +
+               "которое представляет собой смысловую, содержательную и структурную целостность.";
 
-        a = a.replaceAll(" ", ".");
-        String[] words = a.split("\\.");
-        System.out.println(Arrays.toString(words));
+        Pattern email = Pattern.compile("(\\w+)@(gmail|mail|yandex)\\.(com|ru)");
+        Matcher matcher = email.matcher(text);
+        while (matcher.find()){
+            System.out.println(matcher.group(2));
+        }
     }
 }
