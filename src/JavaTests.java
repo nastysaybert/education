06bccd1cdb8a38593f8da01f57eda3e038286aa4
nasty <nasty@saybert.ru>
2021-08-16 14:49:28 +0300

    extended Java Lambda

--- src/JavaTests.java
+++ src/JavaTests.java
@@ -6,18 +6,33 @@ import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+interface Executable{
+    int execute( int x, int y);
+}
+
+class Runner{
+    public void run (Executable e){
+        System.out.println(e.execute(10, 5));
+    }
+}
+
 public class JavaTests {
     public static void main(String[] args)  {
-       String text = "Текст – это несколько предложений, " +
-               "связанных a@yandex.ru между собой по смыслу и грамматически. " +
-               "Есть и более «сложная» tom@mail.com формулировка данного определения. " +
-               "Текст – это john@gmail.com последовательное объединение предложений, " +
-               "которое представляет собой смысловую, содержательную и структурную целостность.";
+        Runner runner = new Runner();
 
-        Pattern email = Pattern.compile("(\\w+)@(gmail|mail|yandex)\\.(com|ru)");
-        Matcher matcher = email.matcher(text);
-        while (matcher.find()){
-            System.out.println(matcher.group(2));
-        }
+        runner.run(new Executable() {
+            @Override
+            public int execute(int x, int y) {
+                System.out.println("Hello2");
+                return x + y;
+            }
+        });
+//        runner.run(() -> {
+//            System.out.println("Hello3");
+//            System.out.println("Bye");
+//            return 2;
+//        });
+
+        runner.run((x, y) -> x + y);
     }
 }
